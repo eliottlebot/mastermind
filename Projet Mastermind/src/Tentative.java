bd2de9377f4817a82cda0b344bf501a2605f3613
Eliott Schott <eliott.schott@etu.unistra.fr>
2023-12-04 15:06:34 +0100

    code

--- Projet Mastermind/src/Tentative.java
+++ Projet Mastermind/src/Tentative.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Tentative {
+public class Tentative {
+    private Couleurs[] combinaisonJoueur;
+    public Indice[] indices;
+    public int pionsBienPlaces;
+
+
+    public Tentative(int tailleCombinaison) {
+        combinaisonJoueur = new Couleurs[tailleCombinaison];
+        indices = new Indice[tailleCombinaison];
+        pionsBienPlaces=0;
+    }
+
+    public Couleurs[] getCombinaisonJoueur(){
+        return combinaisonJoueur;
+    }
+
+    public void rempliCombinaisonJoueur(){
+        for(int i =0; i<combinaisonJoueur.length; i++){
+            Couleurs couleur = Couleurs.BLEU;
+            combinaisonJoueur[i]=couleur;
+        }
+    }
+
+    public void augmentePionsBienPlace(){
+        this.pionsBienPlaces++;
+    }
+    public void setIndicesCouleurs(Indice indice, Integer index){
+        this.indices[index]=indice;
+    }
 }
+
